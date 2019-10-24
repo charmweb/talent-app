9ca5042bb9ce3c0c5c5371be9ba3ec8dd7ec3c02
Daniel Talmacel <codeaddict137@gmail.com>
2019-10-24 15:05:18 +0100

    images

---
+++ assets/images/active/Influencers.js
@@ -0,0 +1,85 @@
+const Influencers = [
+  {
+    id: 1,
+    name: "Cheers Ambasador",
+    gender: "male",
+    image: require("./am-male.jpg"),
+    info: "Chick Trend Influencer",
+    earnings: Math.floor(Math.random() * 9999),
+    icon: { uri: "https://picsum.photos/200/300?" + Math.random() }
+  },
+  {
+    id: 2,
+    name: "Arun Sharma",
+    gender: "female",
+    image: require("./arun-sharma.jpg"),
+    info: "Healthy Life Style Influencer",
+    earnings: Math.floor(Math.random() * 9999),
+    icon: { uri: "https://picsum.photos/200/300?" + Math.random() }
+  },
+  {
+    id: 3,
+    name: "Cihan Soysakal",
+    gender: "male",
+    image: require("./cihan-soysakal.jpg"),
+    info: "Eternal Youth Ambasador",
+    earnings: Math.floor(Math.random() * 9999),
+    icon: { uri: "https://picsum.photos/200/300?" + Math.random() }
+  },
+  {
+    id: 4,
+    name: "Danielle Salcius",
+    gender: "female",
+    image: require("./daniel-salcius.jpg"),
+    info: "CoSM Art Promoter ",
+    earnings: Math.floor(Math.random() * 9999),
+    icon: { uri: "https://picsum.photos/200/300?" + Math.random() }
+  },
+  {
+    id: 5,
+    name: "Irina Iriser",
+    gender: "female",
+    image: require("./irina-iriser.jpg"),
+    info: "80's Fashion Photomodel",
+    earnings: Math.floor(Math.random() * 9999),
+    icon: { uri: "https://picsum.photos/200/300?" + Math.random() }
+  },
+  {
+    id: 6,
+    name: "Jonathon Dorofy",
+    gender: "male",
+    image: require("./jonathon-dorofy.jpg"),
+    info: "Tattoo Artist",
+    earnings: Math.floor(Math.random() * 9999),
+    icon: { uri: "https://picsum.photos/200/300?" + Math.random() }
+  },
+  {
+    id: 7,
+    name: "Iosiah Ness",
+    gender: "male",
+    image: require("./josiah-ness.jpg"),
+    info: "Motor-Bike Adveture Promoter",
+    earnings: Math.floor(Math.random() * 9999),
+    icon: { uri: "https://picsum.photos/200/300?" + Math.random() }
+  },
+  {
+    id: 8,
+    name: "Lucas Davies",
+    gender: "male",
+    image: require("./patrick-t-kindt.jpg"),
+    info: "Rock Style Photomodel",
+    earnings: Math.floor(Math.random() * 9999),
+    icon: { uri: "https://picsum.photos/200/300?" + Math.random() }
+  },
+  {
+    id: 9,
+    name: "Wendy Ang",
+    gender: "female",
+    image: require("./wenyang.jpg"),
+    info: "Wealth Ambasador",
+    earnings: Math.floor(Math.random() * 9999),
+    icon: { uri: "https://picsum.photos/200/300?" + Math.random() }
+  }
+];
+
+export default Influencers;
